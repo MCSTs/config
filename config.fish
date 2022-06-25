6474fc686e3f022fc5362762919a7dd246f79d58
MCSTs <96900586+MCSTs@users.noreply.github.com>
2022-06-25 15:47:44 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 export MALLOC_TRACE=memcheck.log
 export EDITOR=/usr/bin/vim
 export FZF_DEFAULT_COMMAND='rg --files'
+export FZF_DEFAULT_OPTS='--height 60% --layout=reverse --border'
 
 alias ra='ranger'
 alias ll='ls -alF'
