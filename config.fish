afda5b7df856baebbbfcfa2c974104173a3aaae5
mcst <mcst@123.com>
2022-06-27 19:50:35 +0800

    new

--- config.fish
+++ config.fish
@@ -1,11 +1,12 @@
-export MALLOC_TRACE=memcheck.log
-export EDITOR=/usr/bin/vim
-export FZF_DEFAULT_COMMAND='fdfind --hidden --follow -E ".git" -E "node_modules" . /home /etc'
-export FZF_DEFAULT_OPTS='--height 60% --layout=reverse --border'
+set -gx MALLOC_TRACE 'memcheck.log'
+set -gx EDITOR '/usr/bin/vim'
+set -gx FZF_DEFAULT_COMMAND '"fdfind --hidden --follow -E ".git" -E "node_modules" . /home /etc"'
+set -gx FZF_DEFAULT_OPTS '"--height 60% --layout=reverse --border"'
 
 alias ra='ranger'
 alias ll='ls -alF'
 alias l='ls -F'
 alias s='neofetch'
 alias cs='cowsay'
+alias lg='lazygit'
 
