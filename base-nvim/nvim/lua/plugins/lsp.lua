d206bec76b4dcd317f52ca79ae8b60d6c3d3f369
menst <menst@github.com>
2023-05-08 00:16:06 +0800

    add cmp-mapping...

--- base-nvim/nvim/lua/plugins/lsp.lua
+++ base-nvim/nvim/lua/plugins/lsp.lua
@@ -19,12 +19,37 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     -- ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<C-h>"] = cmp.mapping.complete(),
     ["<C-l>"] = cmp.mapping.close(),
     ["<C-i>"] = cmp.mapping.abort(),
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    -- ['<CR>'] = cmp.mapping.confirm({ select = true}),
+    ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        else
+            fallback()
+        end
+    end, {
+        "i",
+        "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        else
+            fallback()
+        end
+    end)
 })
 
 
