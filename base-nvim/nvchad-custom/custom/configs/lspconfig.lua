b2bc0a2b9b230c35f765db58acb445f8dc696c0f
menst <menst@github.com>
2023-05-08 00:30:06 +0800

    add nvchad-custom..
    qsphcjsp.

---
+++ base-nvim/nvchad-custom/custom/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+-- local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "clangd", "cmake", "pylsp", "lua_ls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- require'lspconfig'.lua_ls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+
+-- 
+-- lspconfig.pyright.setup { blabla}
